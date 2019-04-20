4890f6f62b62505a4e20273f0809b4022fb3d0fc
Hamdi Çatal <hamdicatal@users.noreply.github.com>
2019-04-20 13:59:15 +0300

    MVC olarak çalışması için middleware ve routemap eklendi eklendi.

--- UdemyAspNetCoreDemoProject/Startup.cs
+++ UdemyAspNetCoreDemoProject/Startup.cs
@@ -15,6 +15,7 @@ namespace UdemyAspNetCoreDemoProject
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +25,15 @@ namespace UdemyAspNetCoreDemoProject
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name:"default",
+                    //istek controller/index/id şeklinde yapılmalı.(id opsiyonel)
+                    template: "{controller=home}/{action=index}/{id?}"
+                    );
+            });
         }
     }
 }
