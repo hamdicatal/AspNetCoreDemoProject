9c9ad78b97dce362add766e2bc8b3f72205c984d
hamdicatal <hamdicatal@outlook.com>
2019-05-02 16:52:45 +0300

    ViewComponent örneği yapıldı.

---
+++ UdemyAspNetCoreDemoProject/Models/StudentListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UdemyAspNetCoreDemoProject.Entities;
+
+namespace UdemyAspNetCoreDemoProject.Models
+{
+    public class StudentListViewModel
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
